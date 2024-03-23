984f88e491aa732f5a54b3db537e41b5015535b2
TFink7 <98671947+TFink7@users.noreply.github.com>
2024-03-23 11:10:28 -0400

    Add project files.

---
+++ Models/LocationData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallPaperGenerator.Models
+{
+    class LocationData
+    {
+        public int Id { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+    }
+}
---
+++ Models/WallpaperInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallPaperGenerator.Models
+{
+    class WallpaperInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ImagePath { get; set; }
+        public string Location { get; set; }
+        public int WeatherDataId { get; set; }
+        public WeatherData WeatherData { get; set; }
+    }
+}
